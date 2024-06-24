29c98920b09b9027fbc2c1a3af10f96a32a43462
searchLight <searchlight@cyberia.net>
2024-06-24 18:11:09 +0500

    Updates

---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+export default function Index() {
+    
+  return (
+      <p id="zero-state">
+        <br />
+        Check out{" "} Our New Contact List Made in React.
+        {" "}
+        <br/>
+        <p id = "zero-state"></p>
+      </p>
+    );
+  }
